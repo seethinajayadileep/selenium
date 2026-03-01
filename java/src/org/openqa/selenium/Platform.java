a82422e0d192a7de7822c9c456e3e232ad36a9f0
seethinajayadileep <seethinajayadileep@hotmail.com>
2026-03-01 10:33:38 +0530

    [java] Guard against NPE in Platform.extractFromSysProperty

--- java/src/org/openqa/selenium/Platform.java
+++ java/src/org/openqa/selenium/Platform.java
@@ -415,7 +415,7 @@ public enum Platform {
    * @return the most likely platform based on given operating system name
    */
   public static Platform extractFromSysProperty(String osName) {
-    return extractFromSysProperty(osName, System.getProperty("os.version"));
+    return extractFromSysProperty(osName, System.getProperty("os.version",""));
   }
 
   /**
@@ -429,6 +429,7 @@ public enum Platform {
    */
   public static Platform extractFromSysProperty(String osName, String osVersion) {
     osName = osName.toLowerCase(Locale.ENGLISH);
+    osVersion = osVersion == null ? "" : osVersion;
     // os.name for android is linux
     if ("dalvik".equalsIgnoreCase(System.getProperty("java.vm.name"))) {
       return Platform.ANDROID;
